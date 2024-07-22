08ea5b1b2ca646d18a7791da6f54c46287d6d3c9
akash-panthri <akashpanthri2000@gmail.com>
2024-07-22 07:05:51 +0530

    Modified login function in controllers/main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,23 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
 const login = async (req, res)=>{
-    res.send('Fake Login/Register/Signup Route')
+    const {username, password} = req.body
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+const id = new Date().getDate()
+
+
+    const token = jwt.sign({id, password},process.env.JWT_SECRET,{expiresIn:'60d'})
+
+    res.status(200).json({msg:'success', token})
 }
 
 const dashboard = async (req, res)=>{
     const luckyNumber = Math.floor(Math.random() * 100)
-    res.send(200).json({msg:`Hello, Tony Stark `, secret:`Your lucky number is ${luckyNumber}`})
+    res.status(200).json({msg:`Hello, Tony Stark `, secret:`Your lucky number is ${luckyNumber}`})
 }
 
 module.exports = { login, dashboard}
