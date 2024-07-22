cd966f761d168721c54a9e7ed4d245206ed0e5dd
akash-panthri <akashpanthri2000@gmail.com>
2024-07-22 07:22:32 +0530

    Modified dashboard function in controllers/main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,23 +1,45 @@
-const CustomAPIError = require('../errors/custom-error')
-const jwt = require('jsonwebtoken')
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
-const login = async (req, res)=>{
-    const {username, password} = req.body
-    if(!username || !password){
-        throw new CustomAPIError('Please provide email and password', 400)
-    }
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
 
-const id = new Date().getDate()
+  const id = new Date().getDate();
 
+  const token = jwt.sign({ id, password,username }, process.env.JWT_SECRET, {
+    expiresIn: "60d",
+  });
 
-    const token = jwt.sign({id, password},process.env.JWT_SECRET,{expiresIn:'60d'})
+  res.status(200).json({ msg: "success", token });
+};
 
-    res.status(200).json({msg:'success', token})
+const dashboard = async (req, res) => {
+const authHeader = req.headers.authorization
+
+if(!authHeader || !authHeader.startsWith('Bearer ')){
+    throw new CustomAPIError("No token provided", 401);
 }
+const token = authHeader.split(' ')[1]
 
-const dashboard = async (req, res)=>{
-    const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({msg:`Hello, Tony Stark `, secret:`Your lucky number is ${luckyNumber}`})
+
+try {
+    const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    console.log(decoded)
+
+
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res
+    .status(200)
+    .json({
+      msg: `Hello, ${decoded.username} `,
+      secret: `Your lucky number is ${luckyNumber}`,
+    });
+} catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
 }
+};
 
-module.exports = { login, dashboard}
+module.exports = { login, dashboard };
